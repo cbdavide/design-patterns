68105f8306399097e08818db811e21a278607bc7
Andres Angarita <andres.angarita96@gmail.com>
2017-08-09 13:11:18 -0500

    Se arregla falla critica.

--- solid-principles/liskov/src/control/main.java
+++ solid-principles/liskov/src/control/main.java
@@ -14,6 +14,9 @@ public class main {
     public static void main(String[] args) {
         Iterator ite;
         Persona raul = new Persona("raul");
+	
+	System.out.println("yo soy " + raul.getNombre() + " y tengo un perro y una vaca que hacen: ");
+	
         ArrayList<Animal> corralito = raul.getMascotas();
         ite = corralito.iterator();
         while (ite.hasNext()) {
